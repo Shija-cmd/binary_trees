43f714ccd643899aba300836414febe5e4ee0857
Shija-cmd <shijajuma@protonmail.com>
2024-03-29 12:23:40 +0000

    Creating 2-binary_tree_insert_right.c file

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - addind to the right
+ * @parent: parent of the node
+ * @value: node value
+ * Return: NULL or new_node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
+	parent->right = new_node;
+	return (new_node);
+}
